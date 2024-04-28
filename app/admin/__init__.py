76bd8994f14494b494795a9c90ae6ab6576e9980
trezvypapa <mix_mmg@mail.ru>
2024-04-28 13:59:34 +0300

    теперь в админку могут зайти только зарегистрированные пользователи

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,9 +1,19 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user
+
 from app import app, db
 from app.models import User
 
-
 admin = Admin(app)
-admin.add_view(ModelView(User, db.session))
 
+
+class Controller(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return 'У вас нет доступа к админке'
+
+
+admin.add_view(Controller(User, db.session))
