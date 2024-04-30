53f31ed1c43e0d03de530868cf3d6e29fccca54b
trezvypapa <mix_mmg@mail.ru>
2024-04-30 15:53:29 +0300

    снова обновил config.py, чтобы база не стиралась при тестировании

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'jsdfh73410)(*kjfkjanlk'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     FLASK_ADMIN_SWATCH = os.environ.get('FLASK_ADMIN_SWATCH') or 'cerulean'
